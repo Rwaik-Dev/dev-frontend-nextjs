1683474ca81f3f67775ae431c5dd4b9dad6439c3
Rwaik <wilkermid@gmail.com>
2025-07-31 23:42:10 -0300

    feat: Adicionado a listagem dos produtos diretamente na API e servindo os dados na pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
 "use client";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ItemCard from './_components/ItemCard';
+import { ProductTypeResponse } from '@/utils/types';
+import Loader from './_components/Loader';
+
+
 
 export default function Home() {
+
+  const [products, setProducts] = useState<ProductTypeResponse[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchProducts() {
+      setLoading(true)
+      const response = await fetch('http://localhost:3000/api/products/listProducts?limit=1000')
+      const data = await response.json()
+      setProducts(data)
+      setLoading(false)
+    }
+    fetchProducts()
+  }, [])
+
   return (
-    <main className='grid grid-cols-3 gap-4 px-24'>
-      {Array.from({ length: 15 }).map((_, index) => <ItemCard key={index} id={1} title={'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops'} price={109.95} description={'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday'} category={'men\'s clothing'} image={'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'} rating={{
-        rate: 4,
-        count: 125
-      }} />)}
+    <main className={loading ? 'flex justify-center items-center h-screen' : 'grid grid-cols-3 gap-4 px-24'}>
+      {
+        loading ?
+          <Loader /> :
+          products.map((product, index) => <ItemCard key={product.id} id={product.id} title={product.title} price={product.price} description={product.description} category={product.category} image={product.image} rating={product.rating} />)
+      }
     </main>
   )
 }
