07d7fd8ea2b15d13eb224758f1f6acbed61d5090
Rwaik <wilkermid@gmail.com>
2025-08-02 10:33:33 -0300

    feat: Mudado o retorno de erro para apresentação do mesmo para o client atraves do sonner em caso de erro

--- utils/functions/products/viewProductById/index.ts
+++ utils/functions/products/viewProductById/index.ts
@@ -1,23 +1,24 @@
 import { API_URL } from "@/utils/constants";
+import { ProductTypeResponse } from "@/utils/types";
 
 
-export default async function viewProductById({ params }: { params: { id: string } }) {
-    const { id } = params;
+export default async function viewProductById(id:string):Promise<ProductTypeResponse | Error> {
+
     try {
         if (!id) {
-            throw new Error('ID do produto é obrigatório');
+            return new Error('ID do produto é obrigatório');
         }
 
         const response = await fetch(`${API_URL}/products/${id}`);
 
         if (!response.ok) {
-            throw new Error(`Erro ao buscar produto: ${response.statusText}`);
+            return new Error(`Erro ao buscar produto: ${response.statusText}`);
         }
 
         const product = await response.json();
 
         if (!product) {
-            throw new Error('Produto não encontrado');
+            return new Error('Produto não encontrado');
         }
 
         return product;
@@ -25,7 +26,7 @@ export default async function viewProductById({ params }: { params: { id: string
     catch (error) {
         console.error('Erro ao buscar produto:', error);
         const errorMessage = error instanceof Error ? error.message : String(error);
-        throw new Error(errorMessage);
+        return new Error(errorMessage);
     }
 
 }
